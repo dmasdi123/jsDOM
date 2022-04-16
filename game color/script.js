0bc61160d5c9f2f3cb064edea0d2ff45ccc6fdb7
dmasdi123 <febriandimas634@gmail.com>
2022-04-16 16:22:28 +0700

    fixing error

--- game color/script.js
+++ game color/script.js
@@ -20,26 +20,30 @@ tAcakWarna.addEventListener('click', function() {
 });
 
 const sMerah = document.querySelector('input[name=sMerah]');
-const sBiru = document.querySelector('input[name=sBiru]');
 const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
 
-sMerah.addEventListener('change', function() {
+
+sMerah.addEventListener('input', function() {
     const r = sMerah.value;
     const g = sHijau.value;
     const b = sBiru.value;
-    document.body.backgroundColor = `rgb(`+ r +`,`+ g +`,`+ b +`)`;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
 });
 
-sHijau.addEventListener('change', function() {
+
+sHijau.addEventListener('input', function() {
     const r = sMerah.value;
     const g = sHijau.value;
     const b = sBiru.value;
-    document.body.backgroundColor = `rgb(`+ r +`,`+ g +`,`+ b +`)`;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
 });
 
-sBiru.addEventListener('change', function() {
+sBiru.addEventListener('input', function() {
     const r = sMerah.value;
     const g = sHijau.value;
     const b = sBiru.value;
-    document.body.backgroundColor = `rgb(`+ r +`,`+ g +`,`+ b +`)`;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
 });
+
+
